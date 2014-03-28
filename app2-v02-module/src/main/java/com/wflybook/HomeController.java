381b68906ec8e8b4566db32608418c0ba877743c
Your Name <filippespolti@gmail.com>
2014-03-28 16:41:33 -0300

    Packetpub apps

--- app2-v02-module/src/main/java/com/wflybook/HomeController.java
+++ app2-v02-module/src/main/java/com/wflybook/HomeController.java
@@ -1,34 +1,98 @@
 package com.wflybook;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.log4j.Logger;
 
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 @Resource
 public class HomeController {
 
 	private final Result result;
 
-	
-	public HomeController(Result result){
+	public HomeController(Result result) {
 		this.result = result;
 	}
-	
-	@Path("/home")
-	public void home(String level) {
-				
-		
-		Logger.getLogger(getClass()).fatal("This is a fatal message, a high level message error.");
-		Logger.getLogger(getClass()).error("This is a error message.");
-		Logger.getLogger(getClass()).warn("This is a warning message.");
-		Logger.getLogger(getClass()).info("This is a information message.");
-		Logger.getLogger(getClass()).debug("This is a debug message.");
-		Logger.getLogger(getClass()).trace("This is a trace message, very detailed messages.");
-		
-		
-		result.include("oi","oi");
-		
+
+	@Path("/")
+	public void home(String level) throws IOException {
+
+		String s = "";
+
+		Logger.getLogger(getClass()).info("Our test Servlet was accessed");
+		java.util.Properties config = new java.util.Properties();
+		config.put("StrictHostKeyChecking", "no");
+
+		JSch jsch = new JSch();
+		Session session = null;
+		try {
+			session = jsch.getSession("Your-Linux-User", "Your-Linux-Host", 22);
+		} catch (JSchException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		session.setConfig(config);
+		session.setPassword("Your-Linux-Password");
+		try {
+			session.connect(10000);
+		} catch (JSchException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Channel channel = null;
+		try {
+			channel = session.openChannel("exec");
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		((ChannelExec) channel).setCommand("date");
+		((ChannelExec) channel).setErrStream(System.err);
+		InputStream in = channel.getInputStream();
+
+		try {
+			channel.connect();
+		} catch (JSchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		byte[] tmp = new byte[1024];
+		boolean test = true;
+		while (test == true) {
+
+			while (in.available() > 0) {
+
+				int i = in.read(tmp, 0, 1024);
+				if (i < 0) {
+					break;
+				}
+				s += (new String(tmp, 0, i));
+			}
+
+			if (channel.isClosed()) {
+
+				break;
+			}
+		}
+		channel.disconnect();
+		session.disconnect();
+
+		while (s.endsWith(" ")) {
+			s = s.substring(0, -1);
+		}
+		Logger.getLogger(getClass()).info(s);
 	}
+
 }
